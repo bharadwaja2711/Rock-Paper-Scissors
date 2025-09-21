aa69f58f8eec6d68308d78f017c04e78fdc3637e
Manoj Bharadwaja <bharadwaja1.618@gmail.com>
2025-09-21 16:32:29 +0530

    feat: Computer functionality implemented
    Computer functionality of choosing Rock, Paper, Scissors is implemented

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Hello World")
+let getComputerChoice = () => {
+    let choice = Math.random();
+    if(choice >= 0 && choice < 1/3) return "Rock"
+    else if(choice >= 1/3 && choice < 2/3) return "Paper"
+    else return "Scissors"
+}
+
+console.log(getComputerChoice());
