1c44bf48652ccc2955e542a351dde4a0f3618069
Manoj Bharadwaja <bharadwaja1.618@gmail.com>
2025-09-22 07:20:41 +0530

    feat: Determine who won and who lost

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let computerScore = 0;
+let humanScore = 0;
+
 let getComputerChoice = () => {
     let choice = Math.random();
     if(choice >= 0 && choice < 1/3) return "Rock"
@@ -5,13 +8,28 @@ let getComputerChoice = () => {
     else return "Scissors"
 }
 
-console.log("Computer: " + getComputerChoice());
-
 let getHumanChoice = () => {
-    let choice = Number(prompt('Choose 1 - (Rock), 2 - (Paper), 3 - (Scissors)', 0));
+    let choice = Number(prompt('Choose 1 - (Rock), 2 - (Paper), 3 - (Scissors)'));
     if(choice === 1) return "Rock"
     else if(choice === 2) return "Paper"
     else return "Scissors"
 }
 
-console.log("User: " + getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+    if(humanChoice === computerChoice) {
+        return "It's a draw!"
+    } else if((humanChoice === "Rock" && computerChoice === "Scissors") || (humanChoice === "Paper" && computerChoice === "Rock") || (humanChoice === "Scissors" && computerChoice === "Paper")) {
+        humanScore++;
+        return `You win! ${humanChoice} beats ${computerChoice}`;
+    } else {
+        computerScore++;
+        return `You lost! ${computerChoice} beats ${humanChoice}`;
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log("User: ", humanSelection);
+console.log("Computer: ", computerSelection);
+console.log (playRound(humanSelection, computerSelection));
